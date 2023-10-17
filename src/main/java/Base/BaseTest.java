f5ee3fce73113f79eb4af1445c491ab346b178a5
PrabuddhaJayakodi <prabuddhajayakodi@gmail.com>
2023-10-17 23:36:23 +0530

    add the origin project to VCS

---
+++ src/main/java/Base/BaseTest.java
@@ -0,0 +1,24 @@
+package Base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseTest {
+
+    private WebDriver driver;
+
+    public void setup(){
+
+        System.setProperty("webdriver.chrome.driver" , "C:\\Users\\prabuddhajayakodi\\Desktop\\Java automation\\Assignment_prabuddha\\temp_project\\resources\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+
+    }
+
+    public static void main(String[] args){
+
+        BaseTest btest = new BaseTest();
+        btest.setup();
+    }
+}
