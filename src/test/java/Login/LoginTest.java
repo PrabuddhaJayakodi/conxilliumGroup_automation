f87fc146619342f382581f4c39390dfcd71c97f0
PrabuddhaJayakodi <prabuddhajayakodi@gmail.com>
2023-11-01 22:46:01 +0530

    add json file as test data store.

--- src/test/java/Login/LoginTest.java
+++ src/test/java/Login/LoginTest.java
@@ -2,11 +2,13 @@ package Login;
 
 import Base.BaseTest;
 import Pages.LoginPage;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.testng.annotations.Test;
-
+import org.json.simple.parser.ParseException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.text.ParseException;
+//import java.text.ParseException;
 
 public class LoginTest extends BaseTest {
 
@@ -15,17 +17,20 @@ public class LoginTest extends BaseTest {
     public void loginToTheApplication() throws IOException, ParseException {
 
         //get and read jason data from json file.
-     //   JSONParser jsonParser = new JSONParser();
-     //   FileReader reader = new FileReader("C:\\Users\\prabuddhajayakodi\\Desktop\\Java automation\\sample_automation\\src\\jsonfiles\\logindata.json");
-     //   Object obj = jsonParser.parse(reader);
-     //   JSONObject loginJsonObj = (JSONObject) obj;
-
-     //   String uname = (String) loginJsonObj.get("username");
-     //   String password = (String) loginJsonObj.get("password");
 
 
-        String uname = "admin";
-        String password = "admin@123";
+        JSONParser jsonParser = new JSONParser();
+        FileReader reader = new FileReader("C:\\Users\\prabuddhajayakodi\\Desktop\\Java automation\\Assignment_prabuddha\\temp_project\\src\\Data\\logindata.json");
+       Object obj = jsonParser.parse(reader);
+
+        JSONObject loginJsonObj = (JSONObject) obj;
+
+        String uname = (String) loginJsonObj.get("username");
+        String password = (String) loginJsonObj.get("password");
+
+
+      //  String uname = "admin";
+      //  String password = "admin@123";
 
 
         //login to the application by giving username and password
